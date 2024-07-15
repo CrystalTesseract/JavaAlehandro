26cd205bd38703a26bc1a594dcbd52effe5d8d00
CrystalTesseract <sa2006shaz@gmail.com>
2024-07-15 19:12:46 +0300

    Z

--- src/main/java/SergoProj/Person.java
+++ src/main/java/SergoProj/Person.java
@@ -6,14 +6,4 @@ import lombok.Data;
 public class Person {
     private final String name;
     private final int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return name + ':' + age;
-    }
 }
